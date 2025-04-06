efe580b2d46ab022e4133e26a2ca4cf75bbae7dc
Luiz1996 <luiz_flavio_p@hotmail.com>
2025-04-06 12:54:47 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+This is the Tax Calculation Control Service of LFPSys.
+
+#### Main Technology Stack
+* Java 21
+* PostgreSQL
+* Apache Kafka
+* Redis
+* Maven 3.9.1 (or higher)
+* [Spring Boot Framework](https://spring.io/projects/spring-boot)
---
+++ src/main/java/com/lfpsys/lfpsys_tax_calculation_services/LfpsysTaxCalculationServicesApplication.java
@@ -0,0 +1,13 @@
+package com.lfpsys.lfpsys_tax_calculation_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfpsysTaxCalculationServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfpsysTaxCalculationServicesApplication.class, args);
+	}
+
+}
